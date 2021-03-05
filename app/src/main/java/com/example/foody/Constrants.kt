bcdc2a5adc4eadc2f6b9d6d253bb7ece02852829
danieljesus <daniel.j.rodrigues06@gmail.com>
2021-03-05 10:33:22 -0300

    Constrants class

---
+++ app/src/main/java/com/example/foody/Constrants.kt
@@ -0,0 +1,12 @@
+package com.example.foody
+
+class Constrants {
+
+    companion object {
+
+        const val API_KEY = "00cdb575348244d38e4ef12cd655f89a"
+        const val BASE_URL = "https://api.spoonacular.com"
+
+    }
+
+}
