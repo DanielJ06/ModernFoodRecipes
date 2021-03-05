6d65b862f8a96a4230b750f28e9f58b8ba33c4b9
danieljesus <daniel.j.rodrigues06@gmail.com>
2021-03-05 10:07:11 -0300

    Recipe & Food & Ingredients model classes

--- app/src/main/java/com/example/foody/RecipesFragment.kt
+++ app/src/main/java/com/example/foody/RecipesFragment.kt
@@ -8,15 +8,15 @@ import android.view.ViewGroup
 import kotlinx.android.synthetic.main.fragment_recipes.view.*
 
 class RecipesFragment : Fragment() {
-   override fun onCreateView(
+    override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_recipes, container, false)
 
-       view.shimmer_recycler_view.showShimmer()
+        view.shimmer_recycler_view.showShimmer()
 
-       return view
+        return view
     }
 }
