a6b975d828a93fa03f46f05185a19a0ee60aabb6
danieljesus <daniel.j.rodrigues06@gmail.com>
2021-03-09 20:57:21 -0300

    Recipes entity

--- app/src/main/java/com/example/foody/util/Constants.kt
+++ app/src/main/java/com/example/foody/util/Constants.kt
@@ -8,13 +8,16 @@ class Constants {
         const val BASE_URL = "https://api.spoonacular.com"
 
         // API Query Keys
-
         const val QUERY_NUMBER = "number"
         const val QUERY_API_KEY = "apiKey"
         const val QUERY_TYPE = "type"
         const val QUERY_DIET = "diet"
         const val QUERY_ADD_RECIPE_INFORMATION = "addRecipeInformation"
         const val QUERY_FILL_INGREDIENTS = "fillIngredients"
+
+        // ROOM Database
+        const val DB_NAME = "recipes_database"
+        const val RECIPES_TABLE = "recipes_table"
     }
 
 }
